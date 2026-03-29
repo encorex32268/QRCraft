5316a000bb48bfdc444688582d1f72cbcc26928e
LiHan <encorex32268@gmail.com>
2026-03-29 21:39:57 +0800

    Initial commit

---
+++ app/src/main/java/com/lihan/qrcraft/ui/theme/Theme.kt
@@ -0,0 +1,24 @@
+package com.lihan.qrcraft.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColorScheme = lightColorScheme(
+    primary = Primary,
+    surface = Surface,
+    onSurface = OnSurface,
+    error = Error
+)
+
+@Composable
+fun QRCraftTheme(
+    content: @Composable () -> Unit
+) {
+
+    MaterialTheme(
+        colorScheme = LightColorScheme,
+        typography = Typography,
+        content = content
+    )
+}
---
+++ app/src/main/java/com/lihan/qrcraft/ui/theme/Type.kt
@@ -0,0 +1,50 @@
+package com.lihan.qrcraft.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.lihan.qrcraft.R
+
+val SUSE = FontFamily(
+    fonts = listOf(
+        Font(R.font.suse_semibold, FontWeight.SemiBold),
+        Font(R.font.suse_medium, FontWeight.Medium),
+        Font(R.font.suse_regular, FontWeight.Normal),
+    )
+)
+
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    titleMedium = TextStyle(
+        fontFamily = SUSE,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 28.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = SUSE,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 19.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelLarge =  TextStyle(
+        fontFamily = SUSE,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = SUSE,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp
+    )
+)
