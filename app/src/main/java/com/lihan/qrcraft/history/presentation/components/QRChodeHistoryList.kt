2db0d4af642455b85b32f0e8b15d241c57a3f254
LiHan <encorex32268@gmail.com>
2026-04-04 17:13:25 +0800

    Improve user experience , auto scroll top when click favorite, list offscreen gradient

--- app/src/main/java/com/lihan/qrcraft/history/presentation/components/QRChodeHistoryList.kt
+++ app/src/main/java/com/lihan/qrcraft/history/presentation/components/QRChodeHistoryList.kt
@@ -7,16 +7,29 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.CompositingStrategy
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lihan.qrcraft.R
+import com.lihan.qrcraft.core.domain.QRCodeType
 import com.lihan.qrcraft.core.presentation.model.QRCodeHistoryUi
 import com.lihan.qrcraft.ui.theme.OnSurfaceAlt
+import com.lihan.qrcraft.ui.theme.QRCraftTheme
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 @Composable
 fun QRCodeHistoryList(
@@ -32,8 +45,25 @@ fun QRCodeHistoryList(
             modifier = modifier.fillMaxSize()
         )
     } else {
+
+        val listState = rememberLazyListState()
+        val scope = rememberCoroutineScope()
+
         LazyColumn(
-            modifier = modifier.fillMaxSize(),
+            modifier = modifier
+                .fillMaxSize()
+                .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
+                .drawWithContent {
+                    drawContent()
+                    drawRect(
+                        brush = Brush.verticalGradient(
+                            0f to Color.Black,
+                            0.75f to Color.Black,
+                            1f to Color.Transparent
+                        ),
+                        blendMode = BlendMode.DstIn)
+                },
+            state = listState,
             verticalArrangement = Arrangement.spacedBy(8.dp),
             contentPadding = PaddingValues(bottom = 16.dp),
 
@@ -58,6 +88,9 @@ fun QRCodeHistoryList(
                             item.id,
                             item.isFavorite
                         )
+                        scope.launch {
+                            listState.animateScrollToItem(0)
+                        }
                     }
                 )
             }
@@ -78,6 +111,42 @@ fun EmptyView(
             text = text,
             style = MaterialTheme.typography.labelLarge,
             color = OnSurfaceAlt
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun QRCodeHistoryListPreview() {
+    QRCraftTheme {
+        QRCodeHistoryList(
+            items = (0..20).map {
+                QRCodeHistoryUi(
+                    id = it.toLong(),
+                    title = "Google Maps",
+                    type = QRCodeType.entries.random().type,
+                    content = "Content ${it}",
+                    createdAt = System.currentTimeMillis(),
+                    isGenerated = false,
+                    isFavorite = Random.nextBoolean()
+                )
+            },
+            onFavoriteClick = { _, _ -> },
+            onItemLongClick = {},
+            onItemClick = {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun QRCodeHistoryListEmptyPreview() {
+    QRCraftTheme {
+        QRCodeHistoryList(
+            items = emptyList(),
+            onFavoriteClick = { _, _ -> },
+            onItemLongClick = {},
+            onItemClick = {}
         )
     }
 }
